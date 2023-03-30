38088529ed0c30883d1f6a5d056d1e2abb23df1f
whc <772778995@qq.com>
2023-03-30 14:18:32 +0800

    修改拖拽方法

--- abc/js/edit-1-splnum.js
+++ abc/js/edit-1-splnum.js
@@ -34,8 +34,24 @@ const previewScale = () => {
   })
 }
 
+const changeNobrk = () => {
+  $('.nobrk').css({
+    borderRadius: '8px',
+    overflow: 'hidden',
+    boxShadow: '4px 4px 24px 0px rgba(0,22,41,0.14)',
+    background: 'rgb(245, 245, 245)',
+    minHeight: '800px',
+    transform: `scale(${content_vue.m.panzoom.scale / 100})`,
+    transformOrigin: 'center'
+  })
+  $('#target').css({
+    height: $('.nobrk').height() * content_vue.m.panzoom.scale + 'px'
+  })
+}
+
 const changeSign = () => {
   previewScale()
+  changeNobrk()
 
   // $(`g[transform]>use[cat='decos'][type='wedge']`).each((i, item) => {
   //   const el = $(item)
