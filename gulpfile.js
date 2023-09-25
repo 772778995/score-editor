be66249f67ac6628d2cc803f861b8b3d368dc7fb
long <837717627@qq.com>
2023-09-25 15:38:33 +0800

    update：修复速度术语带“on”问题 Signed-off-by: long <837717627@qq.com>

--- gulpfile.js
+++ gulpfile.js
@@ -99,7 +99,7 @@ function buildFile(filePath){
                         removeComments: true, //清除HTML注释
                         collapseWhitespace: true, //压缩HTML
                         collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input checked />
-                        removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+                        // removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
                         removeScriptTypeAttributes: false, //删除<script>的type="text/javascript"
                         removeStyleLinkTypeAttributes: false, //删除<style>和<link>的type="text/css"
                         minifyJS: true, //压缩页面JS
